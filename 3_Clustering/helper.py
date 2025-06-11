0940968decb05eac123a584a1807e4e15afbcf95
Your Name <you@example.com>
2025-06-11 17:27:18 +0200

    Updated Clustering with simulated Datasets

--- 3_Clustering/helper.py
+++ 3_Clustering/helper.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import cycle, islice
+from sklearn import cluster
 
 def normalize(data):
     # Normalize using training set statistics
@@ -157,3 +161,50 @@ def plot_mnist(mnist):
       ax.axis("off")
   plt.suptitle("Sample images from the MNIST dataset", fontsize=16, y=1.05)
   plt.show()
+
+def plot_dbscan_grid(dataset, eps_values, min_samples_values):
+    
+    fig = plt.figure(figsize=(16, 20))
+    plt.subplots_adjust(left=.02, right=.98, bottom=0.001, top=.96, wspace=.05,
+                        hspace=0.25)
+
+
+    plot_num = 1
+
+    for i, min_samples in enumerate(min_samples_values):
+        for j, eps in enumerate(eps_values):
+            ax = fig.add_subplot( len(min_samples_values) , len(eps_values), plot_num)
+
+            dbscan = cluster.DBSCAN(eps=eps, min_samples=min_samples)
+            y_pred_2 = dbscan.fit_predict(dataset)
+
+            colors = np.array(list(islice(cycle(['#df8efd', '#78c465', '#ff8e34',
+                                                 '#f65e97', '#a65628', '#984ea3',
+                                                 '#999999', '#e41a1c', '#dede00']),
+                                          int(max(y_pred_2) + 1))))
+            colors = np.append(colors, '#BECBD6')
+
+
+            for point in dataset:
+                circle1 = plt.Circle(point, eps, color='#666666', fill=False, zorder=0, alpha=0.3)
+                ax.add_artist(circle1)
+
+            ax.text(0, -0.03, 'Epsilon: {} \nMin_samples: {}'.format(eps, min_samples), transform=ax.transAxes, fontsize=16, va='top')
+            ax.scatter(dataset[:, 0], dataset[:, 1], s=50, color=colors[y_pred_2], zorder=10, edgecolor='black', lw=0.5)
+
+
+            plt.xticks(())
+            plt.yticks(())
+
+            # Calculate the limits for the plot
+            x_min, x_max = dataset[:, 0].min() - 1, dataset[:, 0].max() + 1
+            y_min, y_max = dataset[:, 1].min() - 1, dataset[:, 1].max() + 1
+            plt.xlim(x_min, x_max)
+            plt.ylim(y_min, y_max)
+
+            #plt.xlim(-14, 5)
+            #plt.ylim(-12, 7)
+
+            plot_num = plot_num + 1
+
+    plt.show()
